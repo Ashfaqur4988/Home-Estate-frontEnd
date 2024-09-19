363340ef617440c981700a2eed8936cf94a7449e
Ashfaqur Riaz <ashfakurriaz@gmail.com>
2024-09-19 23:36:27 +0530

    form handling done

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,26 +1,50 @@
 import "./login.scss";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
     <div className="login">
-      <div className="formContainer">
+      <form
+        noValidate
+        onSubmit={handleSubmit(onSubmit)}
+        className="formContainer"
+      >
         <h1>Log In</h1>
         <input
           type="text"
           placeholder="Username"
-          name="username"
           id="username"
+          {...register("username", {
+            required: "Username is required",
+          })}
         />
+        {errors.username && <p className="errors">{errors.username.message}</p>}
         <input
           type="password"
           placeholder="Password"
-          name="password"
           id="password"
+          {...register("password", {
+            required: "Password is required",
+            pattern: {
+              value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gi,
+              message: `password pattern is appropriate`,
+            },
+          })}
         />
-        <button>Sign In</button>
+        {errors.password && <p className="errors">{errors.password.message}</p>}
+        <button type="submit">Sign In</button>
         <Link to="/signup">Don't have an account? Sign Up here!</Link>
-      </div>
+      </form>
+
       <div className="imageContainer">
         <img src="bg.png" alt="" />
       </div>
--- src/pages/signUpPage/SignUpPage.jsx
+++ src/pages/signUpPage/SignUpPage.jsx
@@ -1,27 +1,63 @@
 import "./signUpPage.scss";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const SignUpPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
     <div className="signUp">
-      <div className="formContainer">
+      <form
+        noValidate
+        onSubmit={handleSubmit(onSubmit)}
+        className="formContainer"
+      >
         <h1>Create an Account</h1>
         <input
           type="text"
           placeholder="Username"
-          name="username"
           id="username"
+          {...register("username", {
+            required: "Username is required",
+          })}
         />
-        <input type="email" name="email" id="email" placeholder="Email" />
+        {errors.username && <p className="errors">{errors.username.message}</p>}
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi,
+              message: "Email is not valid",
+            },
+          })}
+        />
+        {errors.email && <p className="errors">{errors.email.message}</p>}
         <input
           type="password"
           placeholder="Password"
-          name="password"
           id="password"
+          {...register("password", {
+            required: "Password is required",
+            pattern: {
+              value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gi,
+              message: `password pattern is appropriate`,
+            },
+          })}
         />
-        <button>Sign Up</button>
+        {errors.password && <p className="errors">{errors.password.message}</p>}
+        <button type="submit">Sign Up</button>
         <Link to="/login">Already have an account? Log In here!</Link>
-      </div>
+      </form>
+
       <div className="imageContainer">
         <img src="bg.png" alt="" />
       </div>
